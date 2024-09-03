da501f6c5c3417370a4a73bf4f5268ffe7fd1eb0
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-09-03 14:48:08 +0300

    Initial Commit

---
+++ 808/_08App.swift
@@ -0,0 +1,17 @@
+//
+//  _08App.swift
+//  808
+//
+//  Created by Роман on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct _08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
